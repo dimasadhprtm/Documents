bb68e4bb43be8c3ce9aab32d1e385507e448f363
Dimas <adhi.pratama20@gmail.com>
2023-11-02 22:36:43 +0700

    upload

---
+++ Assignment/module-2-dimasadhprtm/style.js
@@ -0,0 +1,4 @@
+document.getElementById("mobile-menu").addEventListener("click", function () {
+  var dropdownMenu = document.getElementById("dropdown-menu");
+  dropdownMenu.classList.toggle("active");
+});
